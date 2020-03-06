9c3e611e16c8e8774f225252424855b32152674a
Alexander Tsvetkov <alexander.tsvetkov.93@gmail.com>
2020-03-06 16:07:12 +0200

    Clean-up interface of application updaters

--- com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/util/ElementUpdater.java
+++ com.sap.cloud.lm.sl.cf.process/src/main/java/com/sap/cloud/lm/sl/cf/process/util/ElementUpdater.java
@@ -41,15 +41,15 @@ public interface ElementUpdater {
 
     }
 
-    static ElementUpdater getUpdater(UpdateBehavior updateBehavior) {
-        if (updateBehavior == UpdateBehavior.REPLACE) {
+    static ElementUpdater getUpdater(UpdateStrategy updateBehavior) {
+        if (updateBehavior == UpdateStrategy.REPLACE) {
             return new AttributeReplacer();
         }
-
         return new AttributeMerger();
     }
 
-    enum UpdateBehavior {
+    enum UpdateStrategy {
         REPLACE, MERGE
     }
+
 }
